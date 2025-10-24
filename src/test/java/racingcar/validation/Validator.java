87d39d2608029965140d9497eb9970e9a6a8d1fc
0hj1hyeon <ojh010911@gmail.com>
2025-10-24 17:26:38 +0900

    feat: 시도 횟수 0 이하의 숫자가 입력 됐을 때 유효성 검사 구현

--- src/test/java/racingcar/validation/Validator.java
+++ src/test/java/racingcar/validation/Validator.java
@@ -23,6 +23,10 @@ public class Validator {
             throw new IllegalArgumentException("시도 횟수는 숫자여야 합니다.");
         }
 
+        if (count <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 1회 이상이어야 합니다.");
+        }
+
     }
 
 }
