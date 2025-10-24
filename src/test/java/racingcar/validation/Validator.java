60861ae1d697365025b01939e4784908207e4140
0hj1hyeon <ojh010911@gmail.com>
2025-10-24 17:24:37 +0900

    feat: 이름이 6글자 이상일 때 유효성 검사 구현

--- src/test/java/racingcar/validation/Validator.java
+++ src/test/java/racingcar/validation/Validator.java
@@ -8,5 +8,9 @@ public class Validator {
             throw new IllegalArgumentException("이름은 공백이거나 비어있을 수 없습니다.");
         }
 
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+        }
+
     }
 }
