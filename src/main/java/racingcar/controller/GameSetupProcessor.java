3ee49590558d36d57a0cd8939d7595e5e2c2c6a5
0hj1hyeon <ojh010911@gmail.com>
2025-10-24 23:18:49 +0900

    feat: 컨트롤러 입력 처리 구현

---
+++ src/main/java/racingcar/controller/GameSetupProcessor.java
@@ -0,0 +1,25 @@
+package racingcar.controller;
+
+import racingcar.domain.RacingGame;
+import racingcar.view.IOView;
+
+import java.util.List;
+
+public class GameSetupProcessor {
+
+    private final IOView view;
+
+    public GameSetupProcessor(IOView view) {
+        this.view = view;
+    }
+
+    public RacingGame setupGame() {
+
+        List<String> carNames = InputParser.parseCarNames(view.inputCarNames());
+        return new RacingGame(carNames);
+    }
+
+    public int inputTryCount(){
+        return InputParser.parseTryCount(view.inputTryCount());
+    }
+}
